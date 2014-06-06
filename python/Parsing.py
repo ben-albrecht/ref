b3e3db4d78b2f1dfa879249c846a350fb1fe03e8
ben <benalbreck@gmail.com>
2014-06-06 13:32:51 -0400

    added arma info and python parsing info

--- python/Parsing.py
+++ python/Parsing.py
@@ -3,10 +3,12 @@
 
 # Let's try some Q-Chem outputs
 
+energies=["Full PBE: ", "PBE-in-CCSD(T): ", "Full CCSD(T): "]
 qcoutput = open("h2odim.out")
 for line in qcoutput :
     fields = line.split()
     if "Convergence" in line :
         print fields[1]
+        
 
 qcoutput.close()
